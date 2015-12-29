e3b32729675d726ca9fbd429061337edb30e2d5c
Kevin Alberts <kevin@kevinalberts.nl>
2015-12-29 01:19:10 +0100

    main:  Added sigint handler

--- oce.py
+++ oce.py
@@ -1,24 +1,46 @@
+import signal
 from logging.config import fileConfig, logging
 
-from PyQt5 import QtWidgets
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication, QMessageBox
+
 from gui_controller.startup import Startup
 from gui_controller.main import MainWindow
 import sys
 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
+    # Add Interrupt handler
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    # Create application
+    app = QApplication(sys.argv)
+
+    # Add timer to make time to handle interrupts
+    timer = QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)  # Let the interpreter run
+
+    # Show the window and start the app
     form = MainWindow()
     form.show()
     app.exec_()
 
 
 def startup():
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     form = Startup()
     form.show()
     app.exec_()
 
+
+def sigint_handler(*args):
+    """Handler for the SIGINT signal."""
+    sys.stderr.write('\r')
+    if QMessageBox.question(None, 'Stopping', "Are you sure you want to quit?",
+                            QMessageBox.Yes | QMessageBox.No,
+                            QMessageBox.No) == QMessageBox.Yes:
+        QApplication.quit()
 
 if __name__ == "__main__":
     fileConfig('logging.conf')
